f85b1a6de1dcd77a2b36015ca1f66a4408197e69
SonuThapaMagar <sonumagar787@gmail.com>
2025-04-08 09:54:06 +0545

    User Read command created

--- uba-ims/bin/app.js
+++ uba-ims/bin/app.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
-const { program } = require('commander');
-const userCreate =require('../src/commands/userCreate')
+const { program } = require("commander");
+const userCreate = require("../src/commands/userCreate");
+const userList =require('../src/commands/userList')
 
 program
   .name("uba-ims")
@@ -11,8 +12,15 @@ program
 program
   .command("user:create")
   .description("Create a new user")
-  .action((options) => userCreate(options))
   .requiredOption("--fname <firstname>", "User first name")
-  .requiredOption("--lname <lastname>", "User last name");
+  .requiredOption("--lname <lastname>", "User last name")
+  .action((options) => userCreate(options));
 
+//User List / Read command
+program
+  .command("user:read")
+  .description("List all users or search by name")
+  .option("--fname <firstname>", "Search by first name")
+  .option("--lname <lastname>", "Search by last name")
+  .action((options) => userList(options));
 program.parse(process.argv);
---
+++ uba-ims/src/commands/userList.js
@@ -0,0 +1,30 @@
+const { getUsers } = require("../helpers/dbHelper");
+
+module.exports = async (options) => {
+  try {
+    const users = await getUsers();
+    if (options.fname || options.lname) {
+      const filteredUsers = users.filter((user) => {
+        return (
+          (!options.fname ||
+            user.fname.toLowerCase().includes(options.fname.toLowerCase())) &&
+          (!options.lname ||
+            user.lname.toLowerCase().includes(options.lname.toLowerCase()))
+        );
+      });
+
+      if (filteredUsers.length === 0) {
+        console.log("No users found.");
+        return;
+      }
+      console.log("Users:");
+      filteredUsers.forEach((user) => {
+        console.log(
+          `ID: ${user.id}, Name: ${user.fname} ${user.lname}, Created: ${user.createdAt}`
+        );
+      });
+    }
+  } catch (error) {
+    console.error("Error reading users:", error.message);
+  }
+};
