fbe1c8fe76a9c15096be35904367c27fae43c971
SonuThapaMagar <sonumagar787@gmail.com>
2025-04-22 20:45:10 +0545

    DB helper converted in TS

---
+++ uba-ims/src/db/dbHelper.ts
@@ -0,0 +1,40 @@
+import fs from "fs-extra";
+import path from "path";
+//Custom interface
+import { User } from "../interfaces/User";
+
+//JSON db path
+const dbPath = path.join(__dirname, "../data/users.json");
+
+//DB Check
+export async function initDB():Promise<void> {
+  try {
+    //Creating a file if it doesn't exists
+    await fs.ensureFile(dbPath);
+    const currentData = await fs.readFile(dbPath, "utf8");
+    if (currentData.trim() === "") {
+      // Initialize with empty array
+      await fs.writeJson(dbPath, []);
+    }
+  } catch (error) {
+    console.error("Error initializing database:", error);
+    throw error;
+  }
+}
+
+//Listing all users
+export async function getUsers():Promise<User[]> {
+  await initDB();
+  return await fs.readJson(dbPath);
+}
+
+//Saving users
+export async function saveUsers(users:User[]):Promise<void> {
+  await initDB();
+  return await fs.writeJson(dbPath, users, { spaces: 2 });
+}
+
+//Generating ID
+export function generateID():string {
+  return Date.now().toString();
+}
