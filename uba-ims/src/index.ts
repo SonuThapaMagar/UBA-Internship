60b008539485baab8f7483b4fc70be899300af1b
SonuThapaMagar <sonumagar787@gmail.com>
2025-04-22 21:36:46 +0545

    UserDelete converted in TS

--- uba-ims/src/index.ts
+++ uba-ims/src/index.ts
@@ -3,9 +3,12 @@ import { hideBin } from 'yargs/helpers';
 import { userCreate } from "./handler/userCreate";
 import { userList } from "./handler/userList";
 import { userUpdate } from "./handler/userUpdate";
+import { userDelete } from "./handler/userDelete";
 
 //Starting yargs cli
 yargs(hideBin(process.argv))
+    .scriptName("uba-ims")
+    .version("1.0.0")
     .command(
         "user:create",
         "Create a new user",
@@ -36,19 +39,33 @@ yargs(hideBin(process.argv))
         }
     )
     .command(
-        "user:update",
-        "Update User",
+        "user:update <id>",
+        "Update a user by ID",
         (yargs) => {
             return yargs
-                .option("id", { type: "string", demandOption: true, describe: "User ID to update" })
-                .option("fname", { type: "string", describe: "First name" })
-                .option("lname", { type: "string", describe: "Last name" });
+                .positional("id", {
+                    describe: "User ID to update",
+                    type: "string"
+                })
+                .option("fname", { type: "string", describe: "New First name" })
+                .option("lname", { type: "string", describe: "New Last name" });
         },
         async (argv) => {
-            await userUpdate(argv.id as string, {
-                fname: argv.fname as string | undefined,
-                lname: argv.lname as string | undefined
-            })
+            await userUpdate(argv.id as string,argv)
+        }
+
+    )
+    .command(
+        "user:delete <id>",
+        "Delete a user by ID",
+        (yargs) => {
+            return yargs.positional("id", {
+                describe: "User ID to delete",
+                type: "string"
+            });
+        },
+        async (argv) => {
+            await userDelete({ id: argv.id as string });
         }
 
     )
