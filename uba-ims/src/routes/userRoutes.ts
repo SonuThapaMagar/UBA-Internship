e1fa6b9893f5bf06d2080b23a0a614c3ec91d423
SonuThapaMagar <sonumagar787@gmail.com>
2025-04-23 20:59:26 +0545

    API

--- uba-ims/src/routes/userRoutes.ts
+++ uba-ims/src/routes/userRoutes.ts
@@ -2,7 +2,25 @@ import express from "express";
 import { getUsers, saveUsers, generateID } from "../db/dbHelper";
 import { User } from '../interfaces/User';
 
-const router = express.Router();
+const router: express.Router = express.Router();
+//Create user
+router.post('/', async (req, res) => {
+
+    try {
+        const users = await getUsers();
+        const newUser: User = {
+            id: generateID(),
+            fname: req.body.fname,
+            lname: req.body.lname,
+        };
+        users.push(newUser);
+        await saveUsers(users);
+        console.log('Created user:', newUser);
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ error: "Failed to create user" });
+    }
+})
 
 
 //Get all users
@@ -15,5 +33,74 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: "Failed to fetch users" });
     }
 });
+
+// Get user by id
+router.get('/:id', async (req, res) => {
+    try {
+        const users = await getUsers();
+        const user = users.find(u => String(u.id) === String(req.params.id));
+
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch user" });
+    }
+});
+
+//Update user
+router.put('/:id', async (req, res) => {
+    try {
+        const users: User[] = await getUsers();
+        const userId = users.findIndex((user) => String(user.id) === String(req.params.id));
+        if (userId === -1) {
+            console.log("User not found with ID:", req.body.id);
+            return;
+        }
+
+        const updatedUser = {
+            ...users[userId],
+            ...req.body,
+            id: users[userId].id
+        };
+
+        users[userId] = updatedUser;
+        await saveUsers(users);
+        res.status(200).json({
+            message: "User updated successfully",
+            user: updatedUser
+        });
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ error: "Failed to update users" });
+    }
+})
+
+//delete user
+router.delete('/:id', async(req, res)=> {
+    try {
+        const users: User[] = await getUsers();
+        const userId = users.findIndex((u) => u.id.toString() === req.params.id.toString());
+
+        if (userId === -1) {
+            return res.status(404).json({ error: `User with ID ${req.params.id} not found.` });
+            console.log("User with ID");
+
+        }
+
+        const deletedUser = users.splice(userId, 1)[0];
+        await saveUsers(users);
+        res.status(200).json({
+            message: `Successfully deleted user: ${deletedUser.fname} ${deletedUser.lname}`,
+            user: deletedUser,
+        });
+
+
+    } catch (error) {
+        console.error("Error deleting user:", error);
+        res.status(500).json({ error: "Failed to delete user" });
+    }
+});
+
+
+
 
 export default router;
--- uba-ims/src/server.ts
+++ uba-ims/src/server.ts
@@ -1,15 +1,19 @@
 import express from 'express'
 import userRouter from './routes/userRoutes';
+import bodyParser from 'body-parser';
 
 export function startServer() {
     const app = express();
     const PORT = 3000;
 
     app.use(express.json());
+    app.use(bodyParser.json());
+
     //Root route
     app.get('/', (req, res) => {
-        res.send('Welcome to the User API. Use /users to access user data.');
+        res.send('API is running. Use /users endpoint');
     });
+
     //Mounting user routes
     app.use("/users", userRouter);
 
