3300c14bd278bf99d8261ae8561e93395f1d8ac6
SonuThapaMagar <sonumagar787@gmail.com>
2025-05-07 14:09:30 +0545

    Test for user delete function

--- test/controller/userController.test.ts
+++ test/controller/userController.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, vi, beforeEach, expect } from 'vitest';
 import { UserController } from '../../src/controller/UserController';
 import { userService } from '../../src/services/userService';
-import { userList } from '../../src/handler/userList';
+import { send } from 'process';
 
 vi.mock('../../src/services/userService', () => {
     return {
@@ -25,7 +25,8 @@ describe('UserController', () => {
         req = {};
         res = {
             status: vi.fn().mockReturnThis(),
-            json: vi.fn()
+            json: vi.fn(),
+            send: vi.fn(),
         };
         vi.clearAllMocks();
     });
@@ -53,16 +54,44 @@ describe('UserController', () => {
         expect(res.json).toHaveBeenCalledWith(mockUsers);
     })
 
-    it('should get the users by id',async()=>{
-        const mockUser={ id: '1', name: 'John' };
+    it('should get the users by id', async () => {
+        const mockUser = { id: '1', name: 'John' };
         (userService.getUserById as any).mockResolvedValue(mockUser);
 
         req.params = { id: '1' };
-        await controller.getUserById(req,res);
-        
+        await controller.getUserById(req, res);
+
         expect(userService.getUserById).toHaveBeenCalled();
         expect(res.status).toBeCalledWith(200);
         expect(res.json).toHaveBeenCalledWith(mockUser);
+
+    })
+
+    it('should update the users by id', async () => {
+        const mockUpdatedUser = { id: '1', name: 'Update name' };
+        (userService.updateUser as any).mockResolvedValue(mockUpdatedUser);
+
+        req.params = { id: '1' };
+        req.body = { name: 'Updated Name' };
+
+        await controller.userUpdate(req, res);
+
+        expect(userService.updateUser).toHaveBeenCalledWith('1', { name: 'Updated Name' });
+        expect(res.status).toBeCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith(mockUpdatedUser);
+
+    })
+
+    it('should delete the users by id', async () => {
+        (userService.deleteUser as any).mockResolvedValue(undefined);
+
+        req.params = { id: '1' };
+
+        await controller.userDelete(req, res);
+
+        expect(userService.deleteUser).toHaveBeenCalledWith('1');
+        expect(res.status).toBeCalledWith(204);
+        expect(res.send).toHaveBeenCalled();
 
     })
 });
